d833e38d1943245350c7623b93fcdb9a3c587671
Mike <mike@zend.co.za>
2025-05-29 18:59:41 +0100

    added enabler

--- esphome/__main__.py
+++ esphome/__main__.py
@@ -23,7 +23,7 @@ import argcomplete
 # in the built-in version being used instead of the external component one.
 from esphome import const
 import esphome.codegen as cg
-from esphome.config import iter_component_configs, read_config, strip_default_ids
+from esphome.config import iter_component_configs, read_config, strip_default_ids, prev_has_enabler
 from esphome.const import (
     ALLOWED_NAME_CHARS,
     ARGUMENT_HELP_DEVICE,
@@ -676,9 +676,31 @@ def run_miniterm(config: ConfigType, port: str, args) -> int:
 def _wrap_to_code(name, comp, yaml_util):
     coro = coroutine(comp.to_code)
 
+
     @functools.wraps(comp.to_code)
     async def wrapped(conf):
+        
+        import esphome.config as cconf    
+
+        #global prev_has_enabler
+
+        if(cconf.prev_has_enabler == True):
+            cg.add(cg.LineComment("testtting ------------ end"))
+            cg.add(cg.RawStatement("}"))
+
+        if "test" in conf:
+            cg.add(cg.LineComment("testtting ---------------start"))
+            cconf.prev_has_enabler = True
+            tag = conf["test"]
+            cconf.enabler_tag = tag
+            cg.add(cg.RawStatement(f"if(disabler_disabler_id->exists(\"{tag}\"))"))
+            cg.add(cg.RawStatement("{"))
+        else:
+            cconf.prev_has_enabler = False
+
+        
         cg.add(cg.LineComment(f"{name}:"))
+        
         if comp.config_schema is not None:
             conf_str = yaml_util.dump(conf)
             conf_str = conf_str.replace("//", "")
@@ -686,6 +708,8 @@ def _wrap_to_code(name, comp, yaml_util):
             conf_str = conf_str.replace("\\\n", "\n")
             cg.add(cg.LineComment(indent(conf_str)))
         await coro(conf)
+
+        
 
     if hasattr(coro, "priority"):
         wrapped.priority = coro.priority
@@ -709,8 +733,11 @@ def generate_cpp_contents(config: ConfigType) -> None:
 
     for name, component, conf in iter_component_configs(CORE.config):
         if component.to_code is not None:
-            coro = _wrap_to_code(name, component, yaml_util)
+            
+            coro = wrap_to_code(name, component)
+            
             CORE.add_job(coro, conf)
+
 
     CORE.flush_tasks()
 
--- esphome/config.py
+++ esphome/config.py
@@ -41,6 +41,9 @@ from esphome.yaml_util import ESPHomeDataBase, ESPLiteralValue, is_secret
 _LOGGER = logging.getLogger(__name__)
 
 
+prev_has_enabler = False
+enabler_tag = ""
+
 def iter_components(config):
     for domain, conf in config.items():
         component = get_component(domain)
